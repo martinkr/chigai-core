654c3252c2f15c8f6fc87738bae3626618114944
Martin Krause <github@mkrause.info>
2018-05-31 15:54:38 +0300

    v1.5.1: fixes https://github.com/martinkr/chigai-core/issues/1

--- test/adapter/puppeteer.spec.js
+++ test/adapter/puppeteer.spec.js
@@ -1,4 +1,3 @@
-
 /**
  * Specs for the puppeteer adapter
  * This module integrates chrome's headless node API
@@ -27,18 +26,22 @@ const crypto = require("crypto");
 
 const thisModulePath = "adapter/puppeteer";
 const thisModule = require("./../../app/" + thisModulePath);
+const imageSize = require("image-size");
 
 const port = 3000;
 const server = require("chigai-mock-server");
 let testServer;
+const defaultWidth = 550;
+const defaultHeight = 500;
 
 /** creates a data-object as argument */
-const createItem = (wait) => {
+const createItem = (object = {}) => {
 	let item = {}
-	item.wait = wait || 0;
+	item.wait = object.wait || 0;
 	item.uri = `http://localhost:${port}/static`;
-	item.viewportWidth = 500;
-	item.viewportHeight = 500;
+	item.viewport = {};
+	item.viewportWidth = object.width || defaultWidth;
+	item.viewportHeight = object.height || defaultHeight;
 	item.hash = crypto.createHash("sha512").update(item.uri + item.viewportWidth + item.viewportHeight).digest("hex");
 	item.regression_item = path.join("./", "screenshots", item.hash + "_regression.png");
 	return item;
@@ -57,13 +60,13 @@ describe(`the module ${thisModulePath}`, () => {
 		await fs.emptyDir(path.join("./", "screenshots"));
 	});
 
-	after(async() => {
+	after(async () => {
 		await fs.emptyDir(path.join("./", "screenshots"));
 	});
 
 	describe("should handle errors", () => {
 
-		it("should throw if there's no location \"item.uri\"", (async() => {
+		it("should throw if there's no location \"item.uri\"", (async () => {
 			try {
 				await thisModule((() => {
 					let ret = createItem();
@@ -71,12 +74,12 @@ describe(`the module ${thisModulePath}`, () => {
 					return ret;
 				})());
 			} catch (error) {
-				return  error.should.be.an.instanceof(Error);
+				return error.should.be.an.instanceof(Error);
 			}
 			throw new Error("should throw");
 		}));
 
-		it("should throw if there's an error in puppeteer", (async() => {
+		it("should throw if there's an error in puppeteer", (async () => {
 			try {
 				await thisModule((() => {
 					let ret = createItem();
@@ -84,7 +87,7 @@ describe(`the module ${thisModulePath}`, () => {
 					return ret;
 				})());
 			} catch (error) {
-				return  error.should.be.an.instanceof(Error);
+				return error.should.be.an.instanceof(Error);
 			}
 		}));
 
@@ -92,27 +95,68 @@ describe(`the module ${thisModulePath}`, () => {
 
 	describe("should work as expected", () => {
 
-		it("should create a screenshot based on the item", (async() => {
+		it("should create a screenshot based on the item", (async () => {
 			let result;
 			result = await thisModule(createItem());
 			result.screenshot.should.be.ok;
 		}));
 
-		it("should wait and create a screenshot based on the item", (async() => {
+		it("should wait and create a screenshot based on the item", (async () => {
 			let time = Date.now();
 			let wait = 5000;
-			result = await thisModule(createItem(wait));
+			await thisModule(createItem({
+				"wait": wait
+			}));
 			(Date.now() - time).should.be.above(wait - 1);
 		}));
 
-		it("should save the screenshot properly", (async() => {
+		it.only("should create a screenshot with the default height", (async () => {
+			let result;
+			result = await thisModule(createItem());
+			console.log("default result.height", result.height, defaultHeight)
+			result.height.should.equal(defaultHeight);
+		}));
+
+		it.only("should create a screenshot with the default width", (async () => {
+			let result;
+			result = await thisModule(createItem());
+			result = imageSize(result.regression_item);
+			console.log("default result.width", result.width, defaultWidth)
+			result.width.should.equal(defaultWidth);
+		}));
+
+		it.only("should create a screenshot with the supplied width", (async () => {
+			let result;
+			let _width = 300;
+			result = await thisModule(createItem({
+				"width": _width
+			}));
+			result = imageSize(result.regression_item);
+			console.log("supplied result.height", result.width, _width)
+			result.width.should.equal(_width);
+		}));
+
+
+		it.only("should create a screenshot with the supplied height", (async () => {
+			let result;
+			let _height = 400;
+			result = await thisModule(createItem({
+				"height": _height
+			}));
+			result = imageSize(result.regression_item);
+			console.log("supplied result.height", result.height, _height)
+			result.height.should.equal(_height);
+		}));
+
+
+		it("should save the screenshot properly", (async () => {
 			let result;
 			result = await thisModule(createItem());
 			result = await fs.pathExists(result.regression_item);
 			result.should.be.ok;
 		}));
 
-		it("should save the screenshot even if theres no viewport specifiec", (async() => {
+		it("should save the screenshot even if theres no viewport specifiec", (async () => {
 			let result;
 			result = await thisModule((() => {
 				let ret = createItem();
